c1dd884fbef086098abfe22ee3b7f10f8b695845
Chakaitos <chakaitos@gmail.com>
2014-01-15 17:52:50 -0500

    Create a notifier for an order placed

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -1,15 +1,15 @@
 class OrderNotifier < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "Jose Borja <from@example.com>"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.order_notifier.received.subject
   #
-  def received
-    @greeting = "Hi"
+  def received(order)
+    @order = order
 
-    mail to: "to@example.org"
+    mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -18,7 +18,7 @@ class OrderNotifier < ActionMailer::Base
   #   en.order_notifier.shipped.subject
   #
   def shipped
-    @greeting = "Hi"
+    @greeting = "Your order has shipped!"
 
     mail to: "to@example.org"
   end
