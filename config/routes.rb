93d3f5d42e820d228c040214d44f1fed95705952
Chakaitos <chakaitos@gmail.com>
2014-01-24 13:40:31 -0500

    Fix bugs and typos

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Depot::Application.routes.draw do
   get 'admin' => "admin#index"
-  
+
   controller :sessions do
     get 'login' => :new
     post 'login' => :create
@@ -20,14 +20,13 @@ Depot::Application.routes.draw do
     resources :orders
     resources :line_items
     resources :carts
-    root 'store/index', as: 'store', via: :all
+    root 'store#index', as: 'store', via: :all
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'store#index', as: 'store'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
